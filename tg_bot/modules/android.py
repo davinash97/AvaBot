968e4711d3a847bf8b35d1e4c6b582f3735606aa
D Avinash Kumar <56125142+davinash97@users.noreply.github.com>
2021-03-04 20:47:53 +0530

    magisk > android : Adding OFox

---
+++ tg_bot/modules/android.py
@@ -0,0 +1,87 @@
+# Feel free to use it, but don't forget to mention credits
+# By DAvinash97
+from requests import get
+from telegram import Bot, Update, ParseMode
+from telegram.ext import Updater, CommandHandler
+from telegram.ext import run_async
+from tg_bot import dispatcher
+
+msg = update.effective_message
+
+@run_async
+def magisk(bot: Bot, update: Update):
+    magisk = {
+        "<b>Stable</b>\n": "master/stable.json",
+        "<b>Beta</b>\n": "master/beta.json",
+            }.items()
+    link = "https://raw.githubusercontent.com/topjohnwu/magisk_files/"
+    output = "<a href='https://topjohnwu.github.io/Magisk/install.html'><b>Guide for New Magisk</b></a>\n"
+    output += "<a href='https://topjohnwu.github.io/Magisk/install.html#custom-recovery'>How to Install New Magisk?</a> \n"
+    output += "\n<b><u>Latest Magisk Releases:</u></b>\n"
+    for types, jsons in magisk:
+        json_links = get(link + jsons).json()
+        output += f"\n{types}" \
+        f"<i>App: <a href='{json_links.get('magisk').get('link')}'>{json_links.get('magisk').get('version')}</a></i> \n" \
+        f"<i>Uninstaller: <a href='{json_links.get('uninstaller').get('link')}'> Zip v{json_links.get('magisk').get('version')}</a></i> \n"
+    
+    msg.reply_text(text=output,
+                   parse_mode=ParseMode.HTML,
+                   disable_web_page_preview=True)
+
+@run_async
+def ofox(bot, update, args: List[str]):
+    link = 'https://api.orangefox.download/v2/device/{}/releases/'
+    query = " ".join(args)
+    if not query:
+        msg.reply_text("Which Country You mean?")
+        return
+    else:
+        mainlink = link.format(query)
+        getlink = get(mainlink).json()
+        output = f"\n<u><b>Ofox recovery for {query.upper()}</b></u>:\n\n"
+        dllink = 'https://dl.orangefox.download/'
+        stable = getlink.get('stable')
+        for downid in stable:
+            version = downid.get('version')
+            downlinks = dllink + downid.get('_id')
+            date = downid.get('date')
+            output += f"{date}\n"
+            output += f"Version : <i><a href='{downlinks}'>{version}</a></i>\n\n"
+        msg.reply_text(text=output,
+                    parse_mode=ParseMode.HTML,
+                    disable_web_page_preview=True)
+
+@run_async
+def listofox(bot: Bot, update: Update):
+    link = 'https://api.orangefox.download/v2/device'
+    getlink = get(link).json()
+    output = "<b>Ofox is currently available for these devices:</b>\n"
+    for devices in getlink:
+        codename = devices.get('codename')
+        output += f"{codename}\n"
+    bot.send_message(chat_id=update.effective_chat.id,
+                text=output,
+                parse_mode=ParseMode.HTML,
+                disable_web_page_preview=True)
+    
+__help__ = """
+*Available commands:*\n
+*Magisk:* 
+• `/magisk`, `/su`, `/root`: fetches latest magisk
+*Ofox:*
+• `/ofox`, `/fox`, `/orangefox`: fetches latest stable ofox if available for your device.
+• `/listdevices` to check if your devices has ofox officially.
+"""
+magisk_handler = CommandHandler(['magisk', 'root', 'su'], magisk)
+ofox_handler = CommandHandler(['ofox', 'fox', 'orangefox'], ofox)
+ofoxlist_handler = CommandHandler(['listofoxdevices', 'root', 'su'], listofox)
+
+dispatcher.add_handler(magisk_handler)
+dispatcher.add_handler(ofox_handler)
+dispatcher.add_handler(ofoxlist_handler)
+
+__mod_name__ = "Android"
+__command_list__ = ["magisk", "root", "su", "ofox", "fox", "orangefox", "listdevices"]
+__handlers__ = [
+    magisk_handler, ofox_handler, ofoxlist_handler
+]
