b415c3f2d347770ba271ecfbd8c72f79a6b564c4
D Avinash Kumar <56125142+davinash97@users.noreply.github.com>
2021-03-05 20:12:27 +0530

    Update github.py

--- tg_bot/modules/github.py
+++ tg_bot/modules/github.py
@@ -25,17 +25,22 @@ def github(bot, update, args: List[str]):
         changelog = get_link.get('body')
         authorname = get_link.get('author').get('login')
         html_url = get_link.get('author').get('html_url')
-        output = f"<b><u>Author: <a href='{html_url}'> {authorname.upper()}</a></u></b>\n"
-        output += f"<b><u><a href='https://github.com/{query}'>{name}</a></u></b>\n\n"
-        output += f"<b>Changelog:</b>\n<i>{changelog}</i>\n\n"
+        tag_name = get_link.get('tag_name')
+        output = f"<b><u>Author</u> : <a href='{html_url}'> <u>{authorname.upper()}</u></a></b>\n\n"
+        output += f"<b><u><a href='https://github.com/{query}'>{name} {tag_name}</a></u></b>\n\n"
+        output += f"<b>Changelog :</b>\n<i>{changelog}</i>\n\n"
         for downloads in asset:
             assetname = downloads.get('name')
             login = downloads.get('uploader').get('login')
             download_url = downloads.get('browser_download_url')
             asset_size = (downloads.get('size')/1024)/1024
-            calculation = int(asset_size)
-            output += f"<a href='{download_url}'>{assetname}</a>\n"
-            output += f"<b>Size:</b> {calculation}MB\n"
+            calculation = f"{'{0:.2f}'.format(asset_size)}"
+            download_count = downloads.get('download_count')
+            created_at = downloads.get('created_at')
+            output += f"<b>Name :</b> <a href='{download_url}'>{assetname}</a>\n"
+            output += f"<b>Download Count: {download_count}</b>\n"
+            output += f"<b>Size :</b> {calculation}MB\n"
+            output += f"<b>Created at:</b> {created_at}\n\n"
         msg.reply_text(text=output,
                         parse_mode=ParseMode.HTML,
                         disable_web_page_preview=True)
