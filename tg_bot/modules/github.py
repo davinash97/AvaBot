d92653cb86c6da7980d37238f40a115c0d5f6425
D Avinash Kumar <56125142+davinash97@users.noreply.github.com>
2021-03-05 18:01:34 +0530

    fassgdgg

--- tg_bot/modules/github.py
+++ tg_bot/modules/github.py
@@ -23,13 +23,18 @@ def github(bot, update, args: List[str]):
         asset = get_link.get('assets')
         name = get_link.get('name')
         authorname = get_link.get('author').get('login')
-        output = f"<b><u>Author: {authorname.upper()}</u></b>\n"
-        output += f"<b><u>{name}</u></b>\n\n"
+        html_url = get_link.get('author').get('html_url')
+        output = f"<b><u>Author: <a href='{html_url}'> {authorname.upper()}</a></u></b>\n"
+        output += f"<b><u><a href='https://github.com/{query}'>{name}</a></u></b>\n\n"
+        output += f"<b>Changelog:</b>\n<i>{changelog}</i>\n\n"
         for downloads in asset:
             assetname = downloads.get('name')
             login = downloads.get('uploader').get('login')
             download_url = downloads.get('browser_download_url')
+            asset_size = (downloads.get('size')/1024)/1024
+            calculation = int(asset_size)
             output += f"<a href='{download_url}'>{assetname}</a>\n"
+            output += f"<b>Size:</b> {calculation}MB\n"
         msg.reply_text(text=output,
                         parse_mode=ParseMode.HTML,
                         disable_web_page_preview=True)
